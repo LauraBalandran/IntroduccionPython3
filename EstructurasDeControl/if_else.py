290f37b12ca04e9bc9d04e918dcd79f7f6b6dd85
Laura Balandrán C <105017275+LauraBalandran@users.noreply.github.com>
2022-09-25 00:03:02 -0500

    nuevos programas

---
+++ EstructurasDeControl/if_else.py
@@ -0,0 +1,9 @@
+# Solicita un número del usuario 
+num = float(input("Ingresa un número ")) 
+# inicia estructura de control 
+if num == 0: 
+    result = "El número es cero" 
+else: 
+    result = "El número es diferente de cero" 
+# muestra el resultado 
+print(result)
